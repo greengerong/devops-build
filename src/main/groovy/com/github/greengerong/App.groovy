2f62107ccdefd81be87232a04e87017c3680756c
polang <grzx2210@163.com>
2017-09-20 23:08:13 +0800

    version

--- src/main/groovy/com/github/greengerong/App.groovy
+++ src/main/groovy/com/github/greengerong/App.groovy
@@ -1,6 +1,7 @@
 package com.github.greengerong
 
 class App {
+    static final VERSION = "1.0.0"
 
     static void main(def args) {
         def phase = args ? args[0] : ''
@@ -8,12 +9,20 @@ class App {
             println "Please input your build phase, like devops [build/sonar/hub/publish/deploy/tag...]"
             System.exit 1
         }
+        dummyVersion phase
         def params = parseParams args
         def factory = new BuildFactory()
         def result = factory.exec phase, params
         if (!result) {
             println 'DevOps build failure, exit with 1.'
             System.exit 1
+        }
+    }
+
+    static def dummyVersion(phase) {
+        if (phase == 'version') {
+            println "DevOps build version ${VERSION}."
+            System.exit 0
         }
     }
 
