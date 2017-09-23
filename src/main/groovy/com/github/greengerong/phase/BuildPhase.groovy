01c72b9f4212a789e3dc2f9190035f9063a9b300
polang <grzx2210@163.com>
2017-09-23 21:52:10 +0800

    pipeline version

--- src/main/groovy/com/github/greengerong/phase/BuildPhase.groovy
+++ src/main/groovy/com/github/greengerong/phase/BuildPhase.groovy
@@ -4,10 +4,8 @@ package com.github.greengerong.phase
 class BuildPhase implements Phase {
 
     def exec(buildConfig, params) {
-        println 'in BuildPhase exec'
-        println "build config: ${buildConfig}"
-        println "build params: ${params}"
-        System.out << "mvn clean install".execute().inputStream
+
+        System.out << buildConfig.build.command.execute().inputStream
 
         return true
     }
---
+++ src/main/groovy/com/github/greengerong/utils/Pipeline.groovy
@@ -0,0 +1,21 @@
+package com.github.greengerong.utils
+
+class Pipeline {
+
+    static def pipelineNumber() {
+        def pipelineNumber = System.getenv('PIPELINE_NUMBER')
+        pipelineNumber ? pipelineNumber : System.getenv('BUILD_NUMBER')
+    }
+
+    static def version() {
+        def pipelineEnv = System.getenv('PIPELINE_ENV')
+        if (['CI', 'DEV'].contains(pipelineEnv.toUpperCase())) {
+            def pipelineName = System.getenv('PIPELINE_NAME')
+            "${pipelineName}-${Pipeline.pipelineNumber()}"
+        } else {
+            def pipelineBranch = System.getenv('PIPELINE_BRANCH')
+            def split = pipelineBranch.split('/')
+            split[split.length - 1]
+        }
+    }
+}
